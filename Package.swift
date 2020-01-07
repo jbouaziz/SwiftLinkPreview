cab4585066ac548c1abbc668d1d13a6c73156bc3
Jonathan Bouaziz <Jonathan9426@gmail.com>
2020-01-07 19:15:26 +0100

    Bumped version

--- Package.swift
+++ Package.swift
@@ -6,8 +6,22 @@
 //  Copyright © 2016 leocardz.com. All rights reserved.
 //
 
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "SwiftLinkPreview"
+    name: "SwiftLinkPreview",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
+    products: [
+        .library(name: "SwiftLinkPreview", targets: ["SwiftLinkPreview"]),
+    ],
+    targets: [
+        .target(name: "SwiftLinkPreview"),
+    ],
+    swiftLanguageVersions: [.v5]
 )
